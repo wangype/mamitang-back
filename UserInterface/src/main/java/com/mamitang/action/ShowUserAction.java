c53f59ec1947b4a69552548f4222ff849104f699
miemie69 <liyueyang_0609@163.com>
2015-09-28 07:21:06 -0700

    2015-9-28 add

--- UserInterface/src/main/java/com/mamitang/action/ShowUserAction.java
+++ UserInterface/src/main/java/com/mamitang/action/ShowUserAction.java
@@ -1,18 +1,10 @@
 package com.mamitang.action;
 
 import com.mamitang.ReturnStatus;
-import com.mamitang.entity.UserEntity;
 import com.mamitang.response.RetResponse;
 import com.mamitang.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.HashMap;
-import java.util.List;
+import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 
 /**
@@ -20,16 +12,17 @@ import java.util.Map;
  * show all the users
  */
 
-@Controller
+
+@RestController
 @RequestMapping(value = "/userinfo")
 public class ShowUserAction {
 
     @Autowired
     private IUserService userService;
 
-    @RequestMapping(value = "/users" , method = RequestMethod.GET)
+    @RequestMapping(value = "/users/{page}/to/{numOfPage}" , method = RequestMethod.GET)
     @ResponseBody
-    public RetResponse showUsers(@RequestParam int page , @RequestParam int numOfPage){
+    public RetResponse showUsers(@PathVariable("page") int page , @PathVariable("numOfPage") int numOfPage){
         RetResponse result = new RetResponse();
         if(page<=0 || numOfPage<1){
             result.setRetMsg("the error parameters");
